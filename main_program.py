ee68413c2cde4ac57fd7b5aa1879e416b30ea079
Dasha <dasha.kordo@mail.ru>
2019-02-06 21:19:35 +0300

    переделан первый уровень, доделана основная часть. код почти рабочий

--- main_program.py
+++ main_program.py
@@ -29,44 +29,95 @@ pygame.init()
 size = width, height = 525, 675
 screen = pygame.display.set_mode(size)
 
+logo = load_image('Labyrinth.png')
+
 game_over = pygame.sprite.Group()  # создание окончания игры
 
-image = load_image('game_over.png')
+g_o_image = load_image('game_over.png')
 game = pygame.sprite.Sprite(game_over)
-game.image = image
+game.image = g_o_image
 game.rect = game.image.get_rect()
 game.rect.top = 0
 game.rect.left = 0
+
+play = pygame.sprite.Group()  # создание кнопки начала
+
+play_image = load_image('play.png')
+play_image = pygame.transform.scale(play_image, (100, 100))
+btn_play = pygame.sprite.Sprite(play)
+btn_play.image = play_image
+btn_play.rect = btn_play.image.get_rect()
+btn_play.rect.top = 400
+btn_play.rect.left = 200
+
+restart = pygame.sprite.Group()  # создание кнопки заново
+
+rest = pygame.sprite.Sprite(restart)
+rest_image = load_image("restart.png")
+rest_image = pygame.transform.scale(rest_image, (200, 100))
+rest.image = rest_image
+rest.rect = rest.image.get_rect()
+rest.rect.left = 50
+rest.rect.top = 270
 
 all_sprites = pygame.sprite.Group()  # создание спрайта
 
 sprite = pygame.sprite.Sprite(all_sprites)
 ball_image = load_image("air_balloon.png")
-ball_image = pygame.transform.scale(ball_image, (30, 30))
+ball_image = pygame.transform.scale(ball_image, (60, 60))
 sprite.image = ball_image
 sprite.rect = sprite.image.get_rect()
 sprite.rect.left = 10
 sprite.rect.top = 5
 
+circle = pygame.sprite.Group()  # создание конца лабиринта
+
+cir = pygame.sprite.Sprite(circle)
+c_image = load_image("circle.png")
+cir.image = c_image
+cir.rect = sprite.image.get_rect()
+cir.rect.left = 455
+cir.rect.top = 560
+
+win = pygame.sprite.Group()  # создание конца лабиринта
+
+w = pygame.sprite.Sprite(win)
+w_image = load_image("win.png")
+w_image = pygame.transform.scale(w_image, (300, 400))
+w.image = w_image
+w.rect = sprite.image.get_rect()
+w.rect.left = 100
+w.rect.top = 100
+
 clock = pygame.time.Clock()
 
 is_game_over = False
+is_first_level_end = False
+is_start = False
+is_win = True
 
 motion = 'stop'
 
 running = True
 while running:
-    borders = borders_first_level
+    if is_first_level_end:
+        borders = borders_second_level
+    else:
+        borders = borders_first_level
 
     screen.fill((255, 255, 255))
 
-    if not is_game_over:
-        borders_first_level.draw(screen)
+    if (not is_game_over) and is_start and not is_win:
+        borders.draw(screen)
         all_sprites.draw(screen)
 
+        circle.draw(screen)
+
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 running = False
+
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     motion = left
@@ -76,6 +127,7 @@ while running:
                     motion = up
                 elif event.key == pygame.K_DOWN:
                     motion = down
+
             elif event.type == pygame.KEYUP:
                 if event.key in [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_DOWN, pygame.K_UP]:
                     motion = stop
@@ -83,20 +135,63 @@ while running:
         if pygame.sprite.spritecollideany(sprite, borders):
             game_over.draw(screen)
             is_game_over = True
-        if motion == right:
-            sprite.rect.x += 5
-        elif motion == left:
-            sprite.rect.x -= 5
-        elif motion == up:
-            sprite.rect.y -= 5
-        elif motion == down:
-            sprite.rect.y += 5
 
-    if is_game_over:
+        if pygame.sprite.spritecollideany(sprite, circle):
+            if is_first_level_end:
+                is_win = True
+                is_start = False
+            else:
+                is_first_level_end = True
+                sprite.rect.left = 5
+                sprite.rect.top = 10
+            break
+
+        if motion == right:
+            sprite.rect.x += 4
+        elif motion == left:
+            sprite.rect.x -= 4
+        elif motion == up:
+            sprite.rect.y -= 4
+        elif motion == down:
+            sprite.rect.y += 4
+
+    elif is_game_over:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouse_sprite = pygame.sprite.Sprite()
+                mouse_sprite.rect = pygame.Rect(*pygame.mouse.get_pos(), 1, 1)
+                collision = pygame.sprite.spritecollide(mouse_sprite, restart, False)
+                if collision:
+                    is_start = True
+                    is_game_over = False
+                    is_first_level_end = False
+                    motion = stop
+                    sprite.rect.left = 10
+                    sprite.rect.top = 5
+
         game_over.draw(screen)
+        restart.draw(screen)
+
+    elif not is_start:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouse_sprite = pygame.sprite.Sprite()
+                mouse_sprite.rect = pygame.Rect(*pygame.mouse.get_pos(), 1, 1)
+                collision = pygame.sprite.spritecollide(mouse_sprite, play, False)
+                if collision:
+                    is_start = True
+        play.draw(screen)
+        screen.blit(logo, (100, 100))
+
+    elif is_win:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        win.draw(screen)
 
     pygame.display.update()
     pygame.display.flip()
--- maps/first_level.py
+++ maps/first_level.py
@@ -16,97 +16,40 @@ screen = pygame.display.set_mode(size)
 
 borders_first_level = pygame.sprite.Group()
 
-Border(0, 0, width, 5)  # создаю граничные стены экрана
+k = 75
+
+Border(k, k, 4 * k, k + 2)  # создаю горизонтальные стены лабиринта. первый слой
+Border(k, 2 * k, 3 * k, 2 * k + 2)  # второй слой
+Border(3 * k, 3 * k, 5 * k, 3 * k + 2)  # третий слой
+Border(k, 3 * k, 2 * k, 3 * k + 2)
+Border(2 * k, 4 * k, 3 * k, 4 * k + 2)  # четвертый слой
+Border(5 * k, 4 * k, 6 * k, 4 * k + 2)
+Border(k, 5 * k, 3 * k, 5 * k + 2)  # пятый слой
+Border(6 * k, 5 * k, 7 * k, 5 * k + 2)
+Border(3 * k, 6 * k, 4 * k, 6 * k + 2)  # шестой слой
+Border(0, 7 * k, 2 * k, 7 * k + 2)  # седьмой слой
+Border(3 * k, 7 * k, 5 * k, 7 * k + 2)
+Border(k, 8 * k, 2 * k, 8 * k + 2)  # восьмой слой
+Border(3 * k, 8 * k, 5 * k, 8 * k + 2)
+
+Border(k, k, k + 2, 2 * k)  # создаю вертикальные стены лабиринта. первый столбец
+Border(k, 3 * k, k + 2, 6 * k)
+Border(2 * k, 3 * k, 2 * k + 2, 4 * k)  # второй столбец
+Border(2 * k, 5 * k, 2 * k + 2, 8 * k)
+Border(3 * k, 2 * k, 3 * k + 2, 3 * k)  # третий столбец
+Border(3 * k, 6 * k, 3 * k + 2, 7 * k)
+Border(3 * k, 8 * k, 3 * k + 2, 9 * k)
+Border(4 * k, k, 4 * k + 2, 2 * k)  # четвертый столбец
+Border(4 * k, 4 * k, 4 * k + 2, 6 * k)
+Border(5 * k, k, 5 * k + 2, 7 * k)  # пятый столбец
+Border(6 * k, k, 6 * k + 2, 6 * k)  # шестой столбец
+Border(6 * k, 7 * k, 6 * k + 2, 9 * k)
+
+
+Border(0, 0, width, 5)
 Border(0, height - 5, width, height)
 Border(0, 0, 5, height)
 Border(width - 5, 0, width, height)
-
-k = 37.5
-Border(0, k, 4 * k, k + 2)  # создаю горизонтальные стены лабиринта. первый слой
-Border(5 * k, k, 10 * k, k + 2)
-Border(11 * k, k, 12 * k, k + 2)
-Border(k, 2 * k, 5 * k, 2 * k + 2)  # второй слой
-Border(6 * k, 2 * k, 11 * k, 2 * k + 2)
-Border(0, 3 * k, 4 * k, 3 * k + 2)  # третий слой
-Border(7 * k, 3 * k, 12 * k, 3 * k + 2)
-Border(k, 4 * k, 5 * k, 4 * k + 2)  # четвертый слой
-Border(6 * k, 4 * k, 7 * k, 4 * k + 2)
-Border(8 * k, 4 * k, 9 * k, 4 * k + 2)
-Border(10 * k, 4 * k, 11 * k, 4 * k + 2)
-Border(13 * k, 4 * k, 14 * k, 4 * k + 2)
-Border(0, 5 * k, 4 * k, 5 * k + 2)  # пятый слой
-Border(5 * k, 5 * k, 6 * k, 5 * k + 2)
-Border(7 * k, 5 * k, 8 * k, 5 * k + 2)
-Border(10 * k, 5 * k, 11 * k, 5 * k + 2)
-Border(k, 6 * k, 5 * k, 6 * k + 2)  # шестой слой
-Border(0, 7 * k, 5 * k, 7 * k + 2)  # седьмой слой
-Border(7 * k, 7 * k, 8 * k, 7 * k + 2)
-Border(9 * k, 7 * k, 10 * k, 7 * k + 2)
-Border(k, 17 * k, 5 * k, 17 * k + 2)  # семнадцатый слой
-Border(6 * k, 17 * k, 13 * k, 17 * k + 2)
-Border(k, 8 * k, 3 * k, 8 * k + 2)  # восьмой слой
-Border(7 * k, 8 * k, 9 * k, 8 * k + 2)
-Border(11 * k, 8 * k, 12 * k, 8 * k + 2)
-Border(0, 9 * k, 2 * k, 9 * k + 2)  # девятый слой
-Border(5 * k, 9 * k, 9 * k, 9 * k + 2)
-Border(2 * k, 10 * k, 3 * k, 10 * k + 2)  # десятый слой
-Border(4 * k, 10 * k, 5 * k, 10 * k + 2)
-Border(10 * k, 10 * k, 11 * k, 10 * k + 2)
-Border(3 * k, 11 * k, 4 * k, 11 * k + 2)  # одиннадцатый слой
-Border(5 * k, 11 * k, 8 * k, 11 * k + 2)
-Border(11 * k, 11 * k, 12 * k, 11 * k + 2)
-Border(3 * k, 12 * k, 4 * k, 12 * k + 2)  # двенадцатый слой
-Border(5 * k, 12 * k, 7 * k, 12 * k + 2)
-Border(10 * k, 12 * k, 11 * k, 12 * k + 2)
-Border(k, 13 * k, 3 * k, 13 * k + 2)  # тринадцатый слой
-Border(6 * k, 13 * k, 8 * k, 13 * k + 2)
-Border(k, 14 * k, 4 * k, 14 * k + 2)  # четырнадцатый слой
-Border(5 * k, 14 * k, 9 * k, 14 * k + 2)
-Border(11 * k, 14 * k, 12 * k, 14 * k + 2)
-Border(k, 15 * k, 13 * k, 15 * k + 2)  # пятнадцатый слой
-Border(3 * k, 16 * k, 8 * k, 16 * k + 2)  # шестнадцатый слой
-Border(9 * k, 16 * k, 13 * k, 16 * k + 2)  # шестнадцатый слой
-
-
-Border(k, 9 * k, k + 2, 13 * k)  # создаю вертикальные стены лабиринта. первый столбец
-Border(k, 15 * k, k + 2, 17 * k)
-Border(2 * k, 10 * k, 2 * k + 2, 13 * k)  # второй столбец
-Border(3 * k, 8 * k, 3 * k + 2, 10 * k)  # третий столбец
-Border(3 * k, 11 * k, 3 * k + 2, 12 * k)
-Border(4 * k, 8 * k, 4 * k + 2, 11 * k)  # четвертый столбец
-Border(4 * k, 12 * k, 4 * k + 2, 14 * k)
-Border(5 * k, 2 * k, 5 * k + 2, 6 * k)  # пятый столбец
-Border(5 * k, 7 * k, 5 * k + 2, 9 * k)
-Border(5 * k, 10 * k, 5 * k + 2, 11 * k)
-Border(5 * k, 12 * k, 5 * k + 2, 14 * k)
-Border(6 * k, 2 * k, 6 * k + 2, 8 * k)  # шестой столбец
-Border(6 * k, 9 * k, 6 * k + 2, 10 * k)
-Border(6 * k, 11 * k, 6 * k + 2, 12 * k)
-Border(6 * k, 17 * k, 6 * k + 2, 18 * k)
-Border(7 * k, 3 * k, 7 * k + 2, 4 * k)  # седьмой столбец
-Border(7 * k, 5 * k, 7 * k + 2, 7 * k)
-Border(7 * k, 8 * k, 7 * k + 2, 9 * k)
-Border(7 * k, 10 * k, 7 * k + 2, 11 * k)
-Border(8 * k, 4 * k, 8 * k + 2, 5 * k)  # восьмой столбец
-Border(8 * k, 6 * k, 8 * k + 2, 7 * k)
-Border(8 * k, 9 * k, 8 * k + 2, 10 * k)
-Border(8 * k, 11 * k, 8 * k + 2, 13 * k)
-Border(8 * k, 16 * k, 8 * k + 2, 17 * k)
-Border(9 * k, 3 * k, 9 * k + 2, 4 * k)  # девятый столбец
-Border(9 * k, 5 * k, 9 * k + 2, 8 * k)
-Border(9 * k, 9 * k, 9 * k + 2, 14 * k)
-Border(9 * k, 15 * k, 9 * k + 2, 16 * k)
-Border(10 * k, 3 * k, 10 * k + 2, 4 * k)  # десятый столбец
-Border(10 * k, 5 * k, 10 * k + 2, 14 * k)
-Border(10 * k, 0, 10 * k + 2, k)
-Border(11 * k, 4 * k, 11 * k + 2, 8 * k)  # одиннадцатый столбец
-Border(11 * k, k, 11 * k + 2, 2 * k)
-Border(11 * k, 9 * k, 11 * k + 2, 10 * k)
-Border(11 * k, 13 * k, 11 * k + 2, 14 * k)
-Border(12 * k, 2 * k, 12 * k + 2, 6 * k)  # двенадцатый столбец
-Border(12 * k, 7 * k, 12 * k + 2, 14 * k)
-Border(13 * k, k, 13 * k + 2, 15 * k)  # тринадцатый столбец
-
 
 if __name__ == '__main__':
     running = True
