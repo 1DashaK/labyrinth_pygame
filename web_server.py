3641bdc88bcc4a26ceec9fdd79b7549a78e643fc
Dasha <da>
2019-03-04 19:27:40 +0300

    опять что-то происходит (реализован гет)

--- web_server.py
+++ web_server.py
@@ -1,38 +1,18 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/sample_file_upload', methods=['POST', 'GET'])
-def sample_file_upload():
+@app.route('/', methods=['POST', 'GET'])
+def main():
     if request.method == 'GET':
-        return '''<!doctype html>
-                        <html lang="en">
-                          <head>
-                            <meta charset="utf-8">
-                            <meta name="viewport"
-                            content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                            <link rel="stylesheet"
-                            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-                            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-                            crossorigin="anonymous">
-                            <title>Пример загрузки файла</title>
-                          </head>
-                          <body>
-                            <h1>Загрузи уровень</h1>
-                            <form method="post" enctype="multipart/form-data">
-                               <div class="form-group">
-                                    <label for="photo">Выберите файл</label>
-                                    <input type="file" class="form-control-file" id="photo" name="file">
-                                </div>
-                                <button type="submit" class="btn btn-primary">Отправить</button>
-                            </form>
-                          </body>
-                        </html>'''
+        return render_template('load.html')
     elif request.method == 'POST':
         f = request.files['file']
-        return "Форма отправлена"
+        levels = [f for f in os.listdir('maps')]
+        num = len(levels)
+        return render_template('new_load.html')
 
 
 if __name__ == '__main__':
